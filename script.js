33f4683a88f11915aed041308f6734bb635c561d
getkrowd-del <getkrowd@gmail.com>
2026-08-12 07:55:02 -0400

    Deploy landing page: Real Estate Wedinar - The Truth About Claude

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Plus Jakarta Sans', 'sans-serif'],
+                        serif: ['Playfair Display', 'serif'],
+                    },
+                    colors: {
+                        brandYellow: '#FCE214',
+                        brandRed: '#EF4444',
+                        brandRedHover: '#DC2626',
+                    }
+                }
+            }
+        }
